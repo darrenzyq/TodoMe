00f18ac32b16c1a2a1ba710700140ca66df3b148
drzhang <578304347@qq.com>
2026-02-06 11:58:39 +0800

    update icon

--- app/src/main/java/com/drzhang/todo/ui/pages/MainScreen.kt
+++ app/src/main/java/com/drzhang/todo/ui/pages/MainScreen.kt
@@ -12,12 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.Restore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -204,7 +204,7 @@ fun TaskItem(
                     SwipeToDismissBoxValue.StartToEnd -> Icons.Default.Delete
                     SwipeToDismissBoxValue.EndToStart -> when {
                         canSwipeToComplete -> Icons.Default.Check
-                        canSwipeToRestore -> Icons.Default.Restore
+                        canSwipeToRestore -> Icons.Default.Build
                         else -> null
                     }
 
